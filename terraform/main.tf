8631639e9c5e3cc20bf852b64d791e6cddd3e004
Manasha Karunanidhi <manasha25062001@gmail.com>
2025-09-25 01:55:16 +0530

    Add Terraform configs and updated GitHub Actions workflow

---
+++ terraform/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = var.region
+}
+
+# EKS Cluster
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  role_arn = var.eks_role_arn
+
+  vpc_config {
+    subnet_ids = var.subnet_ids
+    endpoint_private_access = true
+    endpoint_public_access  = true
+  }
+
+  # Enable CloudWatch logging
+  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+}
+
+# EKS Node Group
+resource "aws_eks_node_group" "this" {
+  cluster_name    = aws_eks_cluster.this.name
+  node_group_name = "${var.cluster_name}-nodegroup"
+  node_role_arn   = var.node_role_arn
+  subnet_ids      = var.subnet_ids
+
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
+  }
+
+  instance_types = ["t3.medium"]
+}
