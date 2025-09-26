34a07b2784e3631de58f1e8e8d25c3523c43b5c9
Manasha Karunanidhi <manasha25062001@gmail.com>
2025-09-26 17:59:55 +0530

    Updated Code

--- main.tf
+++ main.tf
@@ -24,16 +24,3 @@ module "guardduty" {
 output "guardduty_detector_id" {
   value = module.guardduty.guardduty_detector_id
 }
-
-# Outputs
-output "ssm_db_password" {
-  value = module.ssm.db_password
-}
-
-output "ssm_api_key" {
-  value = module.ssm.api_key
-}
-
-output "iam_role_arn" {
-  value = module.iam.provisioner_role_arn
-}
--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "ssm_api_key" {
 output "iam_role_arn" {
   value = module.iam.provisioner_role_arn
 }
+
+# output in root module
+output "guardduty_detector_id" {
+  value = module.guardduty.guardduty_detector_id
+}
